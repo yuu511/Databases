f926702d21bef1037acea06bd151d2e3500bb413
yuu511 <rrrolento@yahoo.com>
2017-07-31 08:14:08 -0700

    Add files via upload

--- lab2/script4.sql
+++ lab2/script4.sql
@@ -5,27 +5,27 @@
 
 /*1.What are the first and last names of all people who are customers of both Downtown Video and
 City Books ?*/
-SELECT dv_customer.first_name, dv_customer.last_name
+SELECT DISTINCT dv_customer.first_name, dv_customer.last_name
 FROM dv_customer, cb_customers
 WHERE  dv_customer.first_name=cb_customers.first_name AND  dv_customer.last_name = cb_customers.last_name;
 
 /*2.What are phone numbers of all people who are customers of both Downtown Video and
 City Books?*/
 
-SELECT dv_address.phone
+SELECT DISTINCT dv_address.phone
 FROM dv_address, cb_customers
-WHERE dv_customer.phone = cb_customers.phone;
+WHERE dv_address.phone = cb_customers.phone;
 
 /*3.What are the first and last names of all customers who live in the district having the
 most customers?*/
 
-SELECT first_name, last_name
+SELECT DISTINCT first_name, last_name
 FROM dv_address, dv_customer
 WHERE dv_address.address_id = dv_customer.address_id AND district =
 (SELECT district
 FROM dv_address
 GROUP BY district
-ORDER BY count(*) DESC
+ORDER BY count(*) ASC
 LIMIT 1);
 
 /*4.What rating is the least commonamong films inthe Downtown Video database, and how
@@ -35,7 +35,7 @@ result.)*/
 SELECT rating,count(a)
 FROM dv_film a
 GROUP BY rating
-ORDER BY count(*) ASC
+ORDER BY count(*) DESC
 LIMIT 1;
 
 /*5.What are the first and last names of the top 10 authors when ranked by the number of
@@ -44,6 +44,7 @@ authors, sorted in descending order)*/
 
 SELECT DISTINCT first_name, last_name, count(a)
 FROM cb_authors, cb_books a 
+GROUP BY a
 WHERE cb_authors.author_id = a.author_id AND a.author_id =  
 (SELECT b.author_id
 FROM cb_books b
