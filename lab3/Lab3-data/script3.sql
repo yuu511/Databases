2e77a53bb0c6b135029b0f06c372352fccedadba
yuu511 <rrrolento@yahoo.com>
2017-08-14 04:32:49 -0700

    Add files via upload

--- lab3/Lab3-data/script3.sql
+++ lab3/Lab3-data/script3.sql
@@ -1,9 +1,14 @@
 ALTER TABLE dv_film
 ADD CONSTRAINT positive_length CHECK (length>0);
 
+UPDATE mg_customers
+SET address_id = 1 WHERE address_id IS NULL;
+
 ALTER TABLE mg_customers
-ALTER COLUMN address_id SET address_id = 1 WHERE address_id IS NULL,
 ADD CONSTRAINT address_notnull CHECK (address_id IS NOT NULL);
+
+UPDATE dv_address
+SET address_id = 1 WHERE address_id IS NULL;
 
 ALTER TABLE dv_address
 ALTER COLUMN address_id SET address_id = 1 WHERE address_id IS NULL,
