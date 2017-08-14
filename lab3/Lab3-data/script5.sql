4e421c1ef848631670831be052a7c6113ccb3c5a
yuu511 <rrrolento@yahoo.com>
2017-08-14 14:13:32 -0700

    Add files via upload

--- lab3/Lab3-data/script5.sql
+++ lab3/Lab3-data/script5.sql
@@ -1,7 +1,7 @@
 /*Elijah Cordova 1425119*/
 /*Lab 3 script5.sql*/
 -- CREATE VIEW customer_city AS
-SELECT first_name,last_name,city_id
+SELECT dv_customer.first_name,dv_customer.last_name,city_id
 FROM mg_customers, dv_address, dv_customer
 WHERE mg_customers.first_name = dv_customer.first_name 
 AND mg_customers.last_name = dv_customer.last_name;
