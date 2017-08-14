1d7eedda64ae4994ed3cf630056300d4c2e6acd8
yuu511 <rrrolento@yahoo.com>
2017-08-14 14:28:24 -0700

    Add files via upload

--- lab3/Lab3-data/script5.sql
+++ lab3/Lab3-data/script5.sql
@@ -4,5 +4,6 @@
 SELECT DISTINCT dv_customer.first_name,dv_customer.last_name,city_id
 FROM dv_customer, cb_customers, dv_address
 WHERE cb_customers.first_name = dv_customer.first_name 
-AND cb_customers.last_name = dv_customer.last_name;
+AND cb_customers.last_name = dv_customer.last_name
+AND dv_customer.address_id = dv_address.address_id;
 
