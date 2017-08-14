fa000b87e7f61bed9b5e4d3459b5e7a8b626ee4e
yuu511 <rrrolento@yahoo.com>
2017-08-14 13:59:15 -0700

    Add files via upload

--- lab3/Lab3-data/script5.sql
+++ lab3/Lab3-data/script5.sql
@@ -1,6 +1,6 @@
 /*Elijah Cordova 1425119*/
 /*Lab 3 script5.sql*/
-CREATE VIEW customer_city AS
+-- CREATE VIEW customer_city AS
 SELECT first_name,last_name,city_id
 FROM mg_customers, dv_address
 WHERE mg_customers.address_id = dv_address.address_id;
