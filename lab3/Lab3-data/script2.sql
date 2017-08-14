3cf8bfe8ceb501411393b471b9545a62d0b2ecb0
yuu511 <rrrolento@yahoo.com>
2017-08-14 03:57:51 -0700

    Add files via upload

--- lab3/Lab3-data/script2.sql
+++ lab3/Lab3-data/script2.sql
@@ -1,5 +1,5 @@
 ALTER TABLE mg_customers
-ADD FOREIGN KEY address_id REFERENCES dv_address(address_id);
+ADD FOREIGN KEY (address_id) REFERENCES dv_address(address_id);
 
 ALTER TABLE cb_books
-ADD FOREIGN KEY author_id REFERENCES cb_authors(author_id);
+ADD FOREIGN KEY (author_id) REFERENCES cb_authors(author_id);
