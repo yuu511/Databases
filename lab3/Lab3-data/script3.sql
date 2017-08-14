1d6dd92ef15fc31782f1a37b35649c3eed72ca7b
yuu511 <rrrolento@yahoo.com>
2017-08-14 04:10:45 -0700

    Add files via upload

--- lab3/Lab3-data/script3.sql
+++ lab3/Lab3-data/script3.sql
@@ -3,7 +3,7 @@ ADD CONSTRAINT positive_length CHECK (length>0);
 
 ALTER TABLE mg_customers
 ALTER COLUMN address_id SET DEFAULT 1,
-ADD CONSTRAINT address_id NOT NULL;
+ADD CONSTRAINT address_id IS NOT NULL;
 
 ALTER TABLE dv_address
-ADD CONSTRAINT address NOT NULL;
+ADD CONSTRAINT address IS NOT NULL;
