ea98c4c5601b44b61793451ad156cfe6fe93a60e
yuu511 <rrrolento@yahoo.com>
2017-08-14 14:47:26 -0700

    Add files via upload

--- lab3/Lab3-data/script5.sql
+++ lab3/Lab3-data/script5.sql
@@ -10,7 +10,13 @@ AND cb_customers.last_name = dv_customer.last_name
 AND dv_customer.address_id = dv_address.address_id;
 
 /*view 2 */
+CREATE VIEW district_stat AS
 SELECT district, COUNT(address_id)
 FROM dv_address
 GROUP BY district
 ORDER BY count(*)ASC;
+
+/*view 3*/
+SELECT first_name, last_name
+FROM cb_authors, cb_books
+WHERE cb_authors.author_id = cb_books.author_id;
