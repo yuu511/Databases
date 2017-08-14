9f2dc1659f4f134997c4fbce67a1a98d894fa80b
yuu511 <rrrolento@yahoo.com>
2017-08-14 14:39:06 -0700

    Add files via upload

--- lab3/Lab3-data/script5.sql
+++ lab3/Lab3-data/script5.sql
@@ -1,6 +1,7 @@
 /*Elijah Cordova 1425119*/
 /*Lab 3 script5.sql*/
 
+/* view 1 */
 CREATE VIEW customer_city AS
 SELECT DISTINCT dv_customer.first_name,dv_customer.last_name,city_id
 FROM dv_customer, cb_customers, dv_address
@@ -8,3 +9,8 @@ WHERE cb_customers.first_name = dv_customer.first_name
 AND cb_customers.last_name = dv_customer.last_name
 AND dv_customer.address_id = dv_address.address_id;
 
+/*view 2 */
+SELECT district, COUNT(address_id)
+FROM dv_address
+GROUP BY district
+ORDER BY count(*)ASC
