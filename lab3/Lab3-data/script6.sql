339f7133456ee0802bf3eef957998e6e9e12b0db
yuu511 <rrrolento@yahoo.com>
2017-08-14 15:43:21 -0700

    Add files via upload

--- lab3/Lab3-data/script6.sql
+++ lab3/Lab3-data/script6.sql
@@ -9,9 +9,16 @@ AND last_name LIKE '%B'
 OR last_name LIKE '%C';
 
 /*2. insert*/
-INSERT INTO dv_address
-VALUES (700,'Koshland','Way','otonokizaka',654,'95064','1234');
+-- INSERT INTO dv_address
+-- VALUES (700,'Koshland','Way','otonokizaka',654,'95064','1234');
 
 /*using address_id created above*/
-INSERT INTO mg_customers
-VALUES (2526,'John','Smith','nigonigo@db.com',700,TRUE);
+-- INSERT INTO mg_customers
+-- VALUES (2526,'John','Smith','nigonigo@db.com',700,TRUE);
+
+/*3. List title of each film whose length exceeds the average length of all films */
+SELECT title, AVG(length) AS average
+FROM dv_film
+GROUP BY title
+WHERE length > average;
+
