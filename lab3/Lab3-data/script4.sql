1b6bee9e25398db144faf4b8c7317404fa3acf04
yuu511 <rrrolento@yahoo.com>
2017-08-14 13:40:45 -0700

    Add files via upload

--- lab3/Lab3-data/script4.sql
+++ lab3/Lab3-data/script4.sql
@@ -25,4 +25,36 @@ VALUES (NULL,'lul','xd','hehexd@db.com',NULL);
 INSERT INTO dv_address
 VALUES (NULL,'lul','xd','hehexd',NULL,'test','test');
 
+/*
+Error dump:
+
+psql:script4.sql:8: ERROR:  update or delete on table "dv_address" violates foreign key constraint "mg_customers_address_id_fkey" on table "mg_customers"
+DETAIL:  Key (address_id)=(47) is still referenced from table "mg_customers".
+
+psql:script4.sql:12: ERROR:  update or delete on table "cb_authors" violates foreign key constraint "cb_books_author_id_fkey" on table "cb_books"
+DETAIL:  Key (author_id)=(1) is still referenced from table "cb_books".
+
+psql:script4.sql:18: ERROR:  new row for relation "dv_film" violates check constraint "positive_length"
+DETAIL:  Failing row contains (1, lul, xd, -12, PG).
+
+psql:script4.sql:22: ERROR:  null value in column "customer_id" violates not-null constraint
+DETAIL:  Failing row contains (null, lul, xd, hehexd@db.com, null, t).
+
+psql:script4.sql:26: ERROR:  null value in column "address_id" violates not-null constraint
+DETAIL:  Failing row contains (null, lul, xd, hehexd, null, test, test).
+
+ */
+
+ /* Legal Insertions */
+INSERT INTO dv_film
+VALUES (1001,'filmtitle1','xd',100,'PG');
+
+INSERT INTO dv_address
+VALUES (2525,'rabu','raidu','otonokizaka',300,'95048','1234');
+
+
+INSERT INTO mg_customers
+VALUES (2525,'nico','yazawa','hehexd@db.com',TRUE);
+
+
 
