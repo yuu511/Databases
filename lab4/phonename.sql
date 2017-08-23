f74421124e783dd690b68ecb0f5ebfb0d1182f0b
yuu511 <rrrolento@yahoo.com>
2017-08-22 17:53:12 -0700

    Add files via upload

--- lab4/phonename.sql
+++ lab4/phonename.sql
@@ -1,5 +1,5 @@
 SELECT phone
 FROM dv_address
 WHERE address_id = (SELECT address_id
-                    FROM mg_customer
+                    FROM mg_customers
                     WHERE first_name= 'John' AND last_name = 'Smith');
