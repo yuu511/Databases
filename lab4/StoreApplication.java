1fa6bef0a1b7ff530fcbc198823cf9f84f325fcd
yuu511 <rrrolento@yahoo.com>
2017-08-22 18:51:33 -0700

    Add files via upload

--- lab4/StoreApplication.java
+++ lab4/StoreApplication.java
@@ -55,7 +55,7 @@ public class StoreApplication {
 	    	System.out.print ("error at getCustomerPhone");
 	    	System.exit(1);
 	     }
-	      System.out.print (result);
+	      System.out.print (result.size());
 
 		return result;
 	}
