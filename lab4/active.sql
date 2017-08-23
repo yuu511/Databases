7729e9d23bee54383fe975b972bd28123e404c41
yuu511 <rrrolento@yahoo.com>
2017-08-22 19:39:01 -0700

    Add files via upload

--- lab4/active.sql
+++ lab4/active.sql
@@ -1,6 +1,5 @@
 SELECT address_id
-FROM  dv_address
-WHERE district = 'Buenos Aires'
-AND dv_address.address_id= (SELECT mg_customers.address_id
-                   FROM mg_customers
-                   WHERE mg_customers.active = 't');
+FROM dv_address
+WHERE  address_id = (SELECT address_id
+                     FROM mg_customers
+                     WHERE active ='t');
