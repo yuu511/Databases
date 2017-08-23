75ecea177ef935712de66618988bda0446d6b2eb
yuu511 <rrrolento@yahoo.com>
2017-08-22 19:40:00 -0700

    Add files via upload

--- lab4/active.sql
+++ lab4/active.sql
@@ -2,4 +2,4 @@ SELECT address_id
 FROM dv_address
 WHERE  address_id = (SELECT address_id
                      FROM mg_customers
-                     WHERE active ='t');
+                     WHERE mg_customers.active ='t');
